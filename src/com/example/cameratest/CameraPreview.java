56a795588bfd1b098f9b83f37d6510bc722eb1aa
jizhen <jizhen@baozou.com>
2015-08-11 14:46:00 +0800

    first commit

---
+++ src/com/example/cameratest/CameraPreview.java
@@ -0,0 +1,76 @@
+package com.example.cameratest;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback{
+
+	private static final String TAG = "CameraPreview";
+	
+	private SurfaceHolder mHolder;
+	private Camera mCamera;
+	
+	public CameraPreview(Context context, Camera camera){
+		super(context);
+		mCamera = camera;
+		
+		mHolder = getHolder();
+		mHolder.addCallback(this);
+		
+		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+	
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		try {
+			mCamera.setPreviewDisplay(holder);
+			mCamera.startPreview();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+		// TODO Auto-generated method stub
+		if(mHolder.getSurface() == null){
+			return;
+		}
+		
+		try{
+			mCamera.stopPreview();
+		} catch(Exception e){
+			
+		}
+		
+		try{
+			mCamera.setPreviewDisplay(holder);
+			mCamera.startPreview();
+		} catch (Exception e){
+			Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+		}
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
